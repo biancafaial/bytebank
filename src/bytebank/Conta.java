c84e28e20fc44c659c105a4d17e3b4daf68ec603
biancafaial <biancafaial@gmail.com>
2019-08-23 10:05:27 -0300

    implemtantação do método de transfêrencia

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -7,15 +7,24 @@ public class Conta {
 	 String titular;
 
 	 void deposita(double valor) {
-		 this.saldo = this.saldo + valor;
+		 this.saldo += valor;
 		 
 	 }
 	 public boolean saca(double valor) {
 		 if(this.saldo >= valor) {
-			 this.saldo = this.saldo - valor;
+			 this.saldo -= valor;
 			 return true;
 		 } else {
 			 return false;
 		 }
 	 }	 
+	 
+	 public boolean tranferencia(double valor, Conta destino) {
+		 if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		 }
+		 return false;
+	 }
 }
--- src/bytebank/TesteMetodo.java
+++ src/bytebank/TesteMetodo.java
@@ -5,10 +5,23 @@ public class TesteMetodo {
 		Conta contaBianca = new Conta();
 		contaBianca.saldo = 100;
 		contaBianca.deposita(50);
+		System.out.println("Valor do saldo Bianca: " + contaBianca.saldo);
 		
-		System.out.println("Valor do saldo Bianca: "+contaBianca.saldo);
-		contaBianca.saca(20);
+		boolean conseguiuRetirar = contaBianca.saca(20);
 		System.out.println(contaBianca.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaKarla = new Conta();
+		contaKarla.saldo = 1000;
+		
+		if(contaKarla.tranferencia(300, contaBianca)) {
+			System.out.println("Tranferencia feita com sucesso");
+		}else {
+			System.out.println("Saldo insuficiente para transferência");
+		}
+		System.out.println("Saldo Atual Karla "+contaKarla.saldo);
+		System.out.println("Saldo Atual Bianca "+contaBianca.saldo);
+		
 	}
 
 }
